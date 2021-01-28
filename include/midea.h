c9cc962d35d5f91e599e5e2c19407bceaada9c6e
Guilherme de Oliveira <oliveira.gui5@gmail.com>
2021-01-27 23:10:48 -0300

    fixed encode function change enums to hex  changed nibble order

--- include/midea.h
+++ include/midea.h
@@ -15,68 +15,71 @@ extern "C"
     midea_tx_buffer_size = 50,
     midea_payload_lenght = 3,
     midea_carrier_frequency = 38000,
-    midea_t_period_us = 500
+    midea_t_period_us = 553
   } MideaSettings;
   // values definitions
 
-  static const rmt_item32_t MIDEA_BIT_0 = {{{midea_t_period_us, 0, midea_t_period_us, 1}}};
-  static const rmt_item32_t MIDEA_BIT_1 = {{{midea_t_period_us, 0, 3 * midea_t_period_us, 1}}};
+  static const rmt_item32_t MIDEA_BIT_0 = {{{midea_t_period_us, 1, midea_t_period_us, 0}}};
+  static const rmt_item32_t MIDEA_BIT_1 = {{{midea_t_period_us, 1, 3 * midea_t_period_us, 0}}};
 
-  static const rmt_item32_t MIDEA_START_SEQUENCE = {{{8 * midea_t_period_us, 0, 8 * midea_t_period_us, 1}}};
+  static const rmt_item32_t MIDEA_START_SEQUENCE = {{{8 * midea_t_period_us, 1, 8 * midea_t_period_us, 0}}};
 
-  static const rmt_item32_t MIDEA_STOP_SEQUENCE = {{{1 * midea_t_period_us, 0, 10 * midea_t_period_us, 1}}};
+  //static const rmt_item32_t MIDEA_STOP_SEQUENCE = {{{1 * midea_t_period_us, 0, 10 * midea_t_period_us, 1}}};
+  static const rmt_item32_t MIDEA_STOP_SEQUENCE = {{{ midea_t_period_us, 1, 10 * midea_t_period_us, 0}}};
 
   typedef rmt_item32_t midea_tx_buffer_t[midea_tx_buffer_size];
-
+ 
   // types definitions
   typedef enum
   {
-    T17C = 0b0000,
-    T18C = 0b0001,
-    T19C = 0b0011,
-    T20C = 0b0010,
-    T21C = 0b0110,
-    T22C = 0b0111,
-    T23C = 0b0101,
-    T24C = 0b0100,
-    T25C = 0b1100,
-    T26C = 0b1101,
-    T27C = 0b1001,
-    T28C = 0b1000,
-    T29C = 0b1010,
-    T30C = 0b1011,
-    T_OFF = 0b1110
+    T17C = 0x0,
+    T18C = 0x1,
+    T19C = 0x3,
+    T20C = 0x2,
+    T21C = 0x6,
+    T22C = 0x7,
+    T23C = 0x5,
+    T24C = 0x4,
+    T25C = 0xC,
+    T26C = 0xD,
+    T27C = 0x9,
+    T28C = 0x8,
+    T29C = 0xA,
+    T30C = 0xB,
+    T_OFF = 0xE
   } MideaTemperature;
 
   typedef enum
   {
-    FAN_AUTO = 0b1011,
-    FAN_LOW = 0b1001,
-    FAN_MED = 0b0101,
-    FAN_HIGH = 0b0011,
-    FAN_OFF = 0b0001
+    FAN_AUTO  = 0xB,
+    FAN_LOW   = 0x9,
+    FAN_MED   = 0x5,
+    FAN_HIGH  = 0x3,
+    FAN_OFF   = 0x1,
   } MideaFan;
 
   typedef enum
   {
-    STATE_ON = 0b1111,
-    STATE_OFF = 0b1011
+    STATE_ON = 0xF,
+    STATE_OFF = 0xB
   } MideaState;
 
   typedef enum
   {
-    COMMAND_COOL = 0b0000,
-    COMMAND_HEAT = 0b1100,
-    COMMAND_AUTO = 0b1000,
-    COMMAND_FAN = 0b0100
-  } MideaCommand;
+    MODE_COOL = 0x0,
+    MODE_HEAT = 0xC,
+    MODE_AUTO = 0x8,
+    MODE_DRY  = 0xD,
+    MODE_FAN  = 0x4,
+    MODE_VENTILATE = 0xF
+  } MideaMode;
 
   typedef struct
   {
     uint8_t protocol_id;
     uint8_t state : 4;
     uint8_t fan : 4;
-    uint8_t command : 4;
+    uint8_t mode : 4;
     uint8_t temperature : 4;
   } MideaFrameData;
 
@@ -88,7 +91,7 @@ extern "C"
 
   // functions declarations
   void initialize_midea_tx_buffer(rmt_item32_t *buffer);
-  void midea_byte2rmt(const uint8_t byte, rmt_item32_t *buffer);
+  void midea_byte2rmt(const uint8_t byte, rmt_item32_t **buffer);
   void midea_encode(uint8_t *data, rmt_item32_t *buffer);
 
 #ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
--- src/midea.c
+++ src/midea.c
@@ -1,40 +1,36 @@
 #include "midea.h"
 
+#define SET_MIDEA_HIGH(X) memcpy((*X)++, &MIDEA_BIT_1, sizeof(rmt_item32_t))
+#define SET_MIDEA_LOW(X) memcpy((*X)++, &MIDEA_BIT_0, sizeof(rmt_item32_t))
+
+
 // functions implementation
 void initialize_midea_tx_buffer(rmt_item32_t *buffer)
 {
+  //clear the buffer
   memset(buffer, 0, sizeof(midea_tx_buffer_t));
   // set the start sequence
   memcpy(buffer, &MIDEA_START_SEQUENCE, sizeof(rmt_item32_t));
   // set the stop sequence
-  memcpy(buffer + 49, &MIDEA_STOP_SEQUENCE, sizeof(rmt_item32_t));
+  memcpy((buffer + 49), &MIDEA_STOP_SEQUENCE, sizeof(rmt_item32_t));
 }
 
-void midea_byte2rmt(const uint8_t byte, rmt_item32_t *buffer)
+void midea_byte2rmt(const uint8_t byte, rmt_item32_t **buffer)
 {
+  //for each bit checks if is 0 or 1
   for (int j = 7; j >= 0; j--)
   {
-    if ((byte >> j) & 1)
-    {
-      memcpy(buffer, &MIDEA_BIT_1, sizeof(rmt_item32_t));
-    }
-    else
-    {
-      memcpy(buffer, &MIDEA_BIT_0, sizeof(rmt_item32_t));
-    }
-    buffer++;
+    ((byte >> j) & 1) ? SET_MIDEA_HIGH(buffer) : SET_MIDEA_LOW(buffer);
   }
 }
 
 void midea_encode(uint8_t *data, rmt_item32_t *buffer)
 {
-  uint8_t index = 1;
   // populate payload in the tx_buffer, always 3 bytes
   for (int i = 0; i < midea_payload_lenght; i++)
   {
-    midea_byte2rmt(*data, &buffer[index]);
-    midea_byte2rmt(~(*data), &buffer[index + 24]);
+    midea_byte2rmt((*data), &buffer);  // encode normal byte
+    midea_byte2rmt(~(*data), &buffer); // encode the inverted byte
     data++;
-    index += 8;
   }
 }
